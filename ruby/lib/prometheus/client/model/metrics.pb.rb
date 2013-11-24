aa8a7bc53c3446ce0cffc30cdd614247d5f5b115
Tobias Schmidt <ts@soundcloud.com>
2013-11-24 19:23:01 +0700

    Add ruby support

---
+++ ruby/lib/prometheus/client/model/metrics.pb.rb
@@ -0,0 +1,86 @@
+## Generated from metrics.proto for io.prometheus.client
+require "beefcake"
+
+module Prometheus
+  module Client
+
+    module MetricType
+      COUNTER = 0
+      GAUGE = 1
+      SUMMARY = 2
+    end
+
+    class LabelPair
+      include Beefcake::Message
+    end
+
+    class Gauge
+      include Beefcake::Message
+    end
+
+    class Counter
+      include Beefcake::Message
+    end
+
+    class Quantile
+      include Beefcake::Message
+    end
+
+    class Summary
+      include Beefcake::Message
+    end
+
+    class Metric
+      include Beefcake::Message
+    end
+
+    class MetricFamily
+      include Beefcake::Message
+    end
+
+    class LabelPair
+      optional :name, :string, 1
+      optional :value, :string, 2
+    end
+
+
+    class Gauge
+      optional :value, :double, 1
+    end
+
+
+    class Counter
+      optional :value, :double, 1
+    end
+
+
+    class Quantile
+      optional :quantile, :double, 1
+      optional :value, :double, 2
+    end
+
+
+    class Summary
+      optional :sample_count, :uint64, 1
+      optional :sample_sum, :double, 2
+      repeated :quantile, Quantile, 3
+    end
+
+
+    class Metric
+      repeated :label, LabelPair, 1
+      optional :gauge, Gauge, 2
+      optional :counter, Counter, 3
+      optional :summary, Summary, 4
+    end
+
+
+    class MetricFamily
+      optional :name, :string, 1
+      optional :help, :string, 2
+      optional :type, MetricType, 3
+      repeated :metric, Metric, 4
+    end
+
+  end
+end
